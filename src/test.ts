632a9372219a99912aacffbdd84a9851c51643ef
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-17 12:03:11 +0700

    feat: Test 6

--- src/test.ts
+++ src/test.ts
@@ -21,67 +21,56 @@ export const persons: Person[] = [
     age: 25,
     occupation: "Chimney sweep",
   },
-  {
-    type: "admin",
-    name: "Jane Doe",
-    age: 32,
-    role: "Administrator",
-  },
-  {
-    type: "user",
-    name: "Kate Müller",
-    age: 23,
-    occupation: "Astronaut",
-  },
-  {
-    type: "admin",
-    name: "Bruce Willis",
-    age: 64,
-    role: "World saver",
-  },
-  {
-    type: "user",
-    name: "Wilson",
-    age: 23,
-    occupation: "Ball",
-  },
-  {
-    type: "admin",
-    name: "Agent Smith",
-    age: 23,
-    role: "Administrator",
-  },
+  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
+  { type: "user", name: "Wilson", age: 23, occupation: "Ball" },
+  { type: "admin", name: "Agent Smith", age: 23, role: "Anti-virus engineer" },
 ]
 
-export const isAdmin = (person: Person): person is Admin =>
-  person.type === "admin"
-export const isUser = (person: Person): person is User => person.type === "user"
-
 export function logPerson(person: Person) {
-  let additionalInformation = ""
-  if (isAdmin(person)) {
-    additionalInformation = person.role
-  }
-  if (isUser(person)) {
-    additionalInformation = person.occupation
-  }
-  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`)
+  console.log(
+    ` - ${person.name}, ${person.age}, ${
+      person.type === "admin" ? person.role : person.occupation
+    }`
+  )
 }
 
-export function filterUsers(
+const getObjectKeys = <T extends Record<string, any>>(obj: T) =>
+  Object.keys(obj) as (keyof T)[]
+
+export function filterPersons(
   persons: Person[],
+  personType: User["type"],
   criteria: Partial<Omit<User, "type">>
-): User[] {
-  return persons.filter(isUser).filter((user) => {
-    const criteriaKeys = Object.keys(criteria) as (keyof Omit<User, "type">)[]
-    return criteriaKeys.every((fieldName) => {
-      return user[fieldName] === criteria[fieldName]
+): User[]
+export function filterPersons(
+  persons: Person[],
+  personType: Admin["type"],
+  criteria: Partial<Omit<Admin, "type">>
+): Admin[]
+export function filterPersons(
+  persons: Person[],
+  personType: Person["type"],
+  criteria: Partial<Person>
+): Person[] {
+  return persons
+    .filter((person) => person.type === personType)
+    .filter((person) => {
+      let criteriaKeys = getObjectKeys(criteria)
+      return criteriaKeys.every((fieldName) => {
+        return person[fieldName] === criteria[fieldName]
+      })
     })
-  })
 }
+
+export const usersOfAge23 = filterPersons(persons, "user", { age: 23 })
+export const adminsOfAge23 = filterPersons(persons, "admin", { age: 23 })
 
 console.log("Users of age 23:")
+usersOfAge23.forEach(logPerson)
 
-filterUsers(persons, {
-  age: 23,
-}).forEach(logPerson)
+console.log()
+
+console.log("Admins of age 23:")
+adminsOfAge23.forEach(logPerson)
